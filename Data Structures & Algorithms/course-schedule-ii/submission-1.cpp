779beef31d7057f5914843ef3532f5601b9d6e71
Yash6163 <yashmailtotrade@gmail.com>
2026-07-22 13:01:48 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        //kahn algo here
+        vector<vector<int>>adjls(n);
+        vector<int>ans;
+        vector<int>indegree(n,0);
+        //prepare adjls and indegree
+        for(auto it : pre){
+            int source=it[1];
+            int dest=it[0];
+            adjls[source].push_back(dest);
+            indegree[dest]++;
+
+        }
+        queue<int>q;
+        //push all 0 indegree in the queue
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        //traverse the queue
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it : adjls[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+                
+            }
+        }
+
+        if(ans.size() != n)return {};
+        
+        return ans;
+    }
+};
